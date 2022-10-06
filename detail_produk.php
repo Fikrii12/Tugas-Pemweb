b0932a482cdae0fd0b5dc8b4f52d161e7d458094
Nurul Fikri <103340289+Fikrii12@users.noreply.github.com>
2022-10-07 06:17:39 +0700

    Add files via upload
    Tugas pemrograman web

---
+++ detail_produk.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TUGAS PEMWEB</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+    <div class="tabel-detail">
+        <table align="center">
+            <tr>
+                <td>DETAIL PRODUK</td>
+            </tr>
+        </table>
+        </div>
+
+        <div class="gambar">
+        <table align="center">
+            <tr align="center">
+                <td><img src="indomi.png" width=40% height=40%></td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+        </div>
+
+        <div class="daftar-detail">
+        <table align="center" >
+            <tr>
+                <td>Harga</td>
+                <td> : </td>
+                <td>3000</td>
+            </tr>
+            <tr>
+                <td>Varian</td>
+                <td> : </td>
+                <td>Mie Goreng</td>
+            </tr>
+        </table>
+        </div>
+    </body>
+</html>
